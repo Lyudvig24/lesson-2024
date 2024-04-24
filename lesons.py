88fedab8a8a640ec1d4ded71dfb466f3bcb94d47
fgopmn <fgopmn1997@mail.ru>
2024-04-24 19:07:40 +0400

    update a file

--- lesons.py
+++ lesons.py
@@ -248,91 +248,60 @@
 
 
 
+import random
+
+def random_words():
+    with open('randomwords.txt','r') as file:
+        words = file.read().split()
+        rand = random.choice(words)
+        return rand
 
 
-
-# def hangman_game(maxTries, word):
-#     letters = []
-#     output = []
-
-#     for let in word:
-#         output.append('*')
-
-#     alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-#     for i in range(1, maxTries + 1):
-#         answer = input("Write Letter from " + ','.join(alphabet) + ' :').upper()
-
-#         if answer in alphabet:
-#             alphabet.remove(answer)
-
-#         hasWon = True
-
-#         if answer in word:
-#             letters.append(answer)
-
-#         for k, w in enumerate(word):
-#             if w not in letters:
-#                 hasWon = False
-
-#             if w == answer:
-#                 output[k] = w
-
-#         print('output', ''.join(output))
-
-#         if hasWon:
-#             print("You Win")
-#             break
-
-#         elif i == maxTries:
-#             print('You Lose')
-
-
-# maxTries = 7
-# word = 'FISH'.upper()
-# hangman_game(maxTries, word)
-
-
-def hangman_game(maxTrires,word):
+def hangman_game(maxTries, word):
     letters = []
     output = []
-    for let in word:
+
+    for _ in word:
         output.append('*')
+    
+    print('initial output', ''.join(output))
 
     alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    for i in range(1,maxTrires+1):
-        answer = input("Write a letter: " + ',' ''.join(alphabet) + ' : ').upper()
+    for i in range(1, maxTries + 1):
+        answer = input("Write Letter from " + ','.join(alphabet) + ' :' ).upper()
+
         if answer in alphabet:
             alphabet.remove(answer)
 
-        hasWon = True
         if answer in word:
             letters.append(answer)
-        for k,w in enumerate(word):
+        
+        hasWon = True
+
+        for k, w in enumerate(word):
             if w not in letters:
                 hasWon = False
 
             if w == answer:
                 output[k] = w
 
-        print("output", ''.join(output))
-
+        print('output', ''.join(output))
+         
         if hasWon:
             print("You Win")
             break
-        elif i == maxTrires:
-            print("You Lose")
 
+        elif i == maxTries:
+            print('You Lose, the word is: ', word)
+    
+
+word = random_words().upper()
 maxTries = 7
-word = 'Laptop'.upper()
-hangman_game(maxTries,word)
 
+if(len(word) > maxTries):
+    maxTries = len(word) + 1
 
-
-
-
-
-
-
+hangman_game(maxTries, word)
 
 
 # def sumarr(mlist):
